2cb3654bc58ec8fa5b1f3fde73d5d180118cd310
sahinmehmet3777 <sahinmehmet377@gmail.com>
2023-04-19 14:12:50 +0300

    Proje dosyası ekle.

---
+++ partialclass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partialclass
+{
+    // Class'a ek yapmak istediğimiz zaman başına partial koyuyoruz, ve class'ın aynısını yazıp sadece eklenicek şeyi içerisine yaz
+    // aynı isimli bu iki class birleşiyot
+    // partial kelimesiyle class kapatılmaz, ileride ek kodlar ekleyebilmemizi sağlar
+    public partial class User
+    {
+        private string name;
+        private string location;
+        public User(string a, string b)
+        {
+            this.name = a;
+            this.location = b;
+        }
+    }
+
+    public partial class User
+    {
+        public void GetUserDetails()
+        {
+            Console.WriteLine("name: " + name);
+            Console.WriteLine("location: " + location);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User u = new User("serdar", "istanbul");
+            u.GetUserDetails();
+            Console.ReadLine();
+        }
+    }
+}
